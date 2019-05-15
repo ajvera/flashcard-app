778c08245428a4be8f23a1840252ca09201733ee
ajvera <adjove@gmail.com>
2019-05-14 17:27:16 -0700

    update route methods to use new db implimentation

--- app.py
+++ app.py
@@ -7,30 +7,28 @@ from tabledef import *
 import json
 from pprint import pprint
 
-app = Flask(__name__)
-
 #Homepage will eventually direct users to their preffered learning style
 @app.route("/")
 def index():
-    session.rollback()
     return render_template('index.html')
 
 #Flashcard review page
 @app.route("/flashcards")
 def get():
-    query = session.query(Flashcard).all()
-    data = []
-    for obj in query:
-        data.append({'id': obj.flashcardID, 'term': obj.term, 'definition': obj.definition})
-    return render_template('show.html', data=data)
+    # query = session.query(Flashcard).all()
+    # data = []
+    # for obj in query:
+    #     data.append({'id': obj.flashcardID, 'term': obj.term, 'definition': obj.definition})
+    flashcards = Flashcard.query.all()
+    return render_template('show.html', data=flashcards)
 
 #Create new flashcard
 @app.route("/flashcards/new", methods=["GET", "POST"])
 def new():
     if request.method == "POST":
         flashcard = Flashcard(term=request.form['term'], definition=request.form['definition'])
-        session.add(flashcard)
-        session.commit()
+        db.session.add(flashcard)
+        db.session.commit()
         return redirect('/flashcards')
     
     return render_template('new.html')
@@ -38,24 +36,23 @@ def new():
 #Update an existing flashcard
 @app.route("/flashcards/update/<id>", methods=["GET","POST"])
 def update(id):
-    flashcard = session.query(Flashcard).get(id)
-    print(Flashcard)
-    
-    
+    flashcard = Flashcard.query.get(id)
+
     if request.method == "POST":
-        session.query(Flashcard).filter(id).update(term=request.form["term"], definition=request.form['definition'])
-        session.commit()
-        return redirect('show.html')
+        flashcard.term = request.form.get('term')
+        flashcard.definition = request.form.get('definition')
+        db.session.commit()
+        return redirect('/flashcards')
 
     return render_template('update.html', flashcard=flashcard)
 
-@app.route("/flashcards/delete")
-def delete():
-    query = session.query(Flashcard).all()
-    data = []
-    for obj in query:
-        data.append({'term': obj.term, 'definition': obj.definition}) 
-    return render_template('index.html', data=data)
+#Deletes Flashcard
+@app.route("/flashcards/delete/<id>")
+def delete(id):
+    flashcard = Flashcard.query.get(id)
+    db.session.delete(flashcard)
+    db.session.commit()
+    return redirect("/flashcards")
 
 if __name__ == "__main__":
     app.debug = True
--- tabledef.py
+++ tabledef.py
@@ -1,25 +1,3 @@
-# from sqlalchemy import create_engine  
-# from sqlalchemy import Column, String, Integer
-# from sqlalchemy.ext.declarative import declarative_base  
-# from sqlalchemy.orm import sessionmaker
-
-# db_string = "postgresql://avera:@+0Ku$@@localhost/flashapp"
-
-# db = create_engine(db_string)
-# base = declarative_base()
-
-# class Flashcard(base):
-#     __tablename__ = 'flashcards'
-
-#     flashcardID = Column(Integer, primary_key=True)
-#     term = Column(String)
-#     definition = Column(String)
-
-# Session = sessionmaker(db)  
-# session = Session()
-
-# base.metadata.create_all(db)
-
 import os
 
 from flask import Flask
